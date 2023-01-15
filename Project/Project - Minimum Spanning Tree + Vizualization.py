29fc90227f4f9462e74dedb61b427b367027c31a
Makaadam11 <83653761+Makaadam11@users.noreply.github.com>
2023-01-15 17:54:26 +0100

    Add files via upload

--- Project/Project - Minimum Spanning Tree + Vizualization.py
+++ Project/Project - Minimum Spanning Tree + Vizualization.py
@@ -3,33 +3,38 @@ import numpy
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
-from pathlib import Path
 
-path = Path(__file__).parent.absolute()
+import os
+path = os.path.abspath(os.path.dirname(__file__))
 
 number = input("Wybierz numer pliku z danymi do oczytania (0-5): ")
 name=""
 
+"""choose dataset
+"""
 if number == '0':
-    name = str(path) +"\data_0"
+    name = os.path.join(path, "data_0.csv")
 elif number == '1':
-    name = str(path) +"\data_1"
+    name = os.path.join(path, "data_1.csv")
 elif number == '2':
-    name = str(path) +"\data_2"
+    name = os.path.join(path, "data_2.csv")
 elif number == '3':
-    name = str(path) +"\data_3"
+    name = os.path.join(path, "data_3.csv")
 elif number == '4':
-    name = str(path) +"\data_4"
+    name = os.path.join(path, "data_4.csv")
 elif number == '5':
-    name = str(path) +"\data_5"
+    name = os.path.join(path, "data_5.csv")
 
-
-parsedCSV = numpy.array(list(csv.reader(open(name + ".csv", "r"), delimiter=",")))[1:]
+"""parsing and saving data from .csv file
+"""
+parsedCSV = numpy.array(list(csv.reader(open(name, "r"), delimiter=",")))[1:]
 points = numpy.array([[float(x), float(y), int(z)] for x, y, z in parsedCSV])
 size = (len(parsedCSV)) 
 
 cluster_method =''
 
+"""choose clustering method
+"""
 cluster_input = input("Wpisz metodę klastrowania: \n1) Funkcja używająca jądra Gaussa (RBF) z odległością euklidesową wpisz: rbf \n2) Funkcja używająca K-macierz łączności najbliższych sąsiadów wpisz: knn\n")
 
 if cluster_input == 'knn':
@@ -39,51 +44,16 @@ if cluster_input == 'rbf':
 
 cluster_number = input("Wpisz ilość klastrów: ")
 
-
+"""
+store result of MTS algorithm
+"""
 result = []
 csv_result = []
 
 def MAX(sets):
     return (max(sets))
 
-# SpectralClustering to metoda agregacji danych, która działa poprzez znajdowanie skupień w danych za pomocą analizy spektralnej
-# macierzy sąsiedztwa. # Jeśli użyjesz parametru `assign_labels='kmeans' algorytm ten działa w następujący sposób:
 
-# 1) Obliczenie macierzy sąsiedztwa: Na początku algorytm oblicza macierz sąsiedztwa dla danych wejściowych. Macierz sąsiedztwa 
-# jest macierzą rzędową, w której wiersze i kolumny odpowiadają poszczególnym próbkom danych, a wartości na jej przekątnej są równe 0. 
-# Wartości poza przekątną są równe 1, jeśli dane próbki są sąsiadami, lub 0, jeśli nie są. Sąsiadami nazywamy próbki, które są blisko siebie według określonej miary odległości.
-
-# 2) Obliczenie macierzy laplasjanu: Następnie algorytm oblicza macierz laplasjanu, która jest pochodną macierzy sąsiedztwa.
-#  Macierz ta jest używana do określenia podobieństwa między próbkami danych.
-
-# 3) Redukcja wymiarów: Następnie algorytm wykonuje redukcję wymiarów na macierzy laplasjanu, aby zmniejszyć wymiar danych wejściowych. 
-# Redukcja wymiarów polega na znalezieniu kombinacji składowych macierzy, które najlepiej opisują dane wejściowe.
-
-# 4) Grupowanie danych: Na końcu algorytm grupuje dane wejściowe za pomocą metody k-średnich (kmeans), 
-# wykorzystując zredukowane wymiary jako cechy. Metoda k-średnich działa poprzez iteracyjne przypisywanie każdej 
-# próbki do najbliższej grupy (centroidu) i aktualizowanie centroidów w oparciu o nowe przypisania.
-
-
-# Jeśli ustawisz parametr affinity na 'rbf' podczas wywoływania funkcji SpectralClustering z biblioteki scikit-learn, 
-# algorytm będzie używać jądrowej funkcji RBF (Radial Basis Function, funkcja bazowa radialna) 
-# do obliczenia macierzy podobieństwa między punktami danych. Jądrowa funkcja RBF to funkcja, która jest znormalizowana tak, 
-# aby mieć wartość równą 1 dla punktu, w którym jest wywoływana, oraz malejącą wartość wraz z oddalaniem się od tego punktu.
-# W praktyce oznacza to, że algorytm SpectralClustering będzie używał macierzy podobieństwa opartej na odległości euklidesowej między punktami danych. 
-# Punkty blisko siebie będą miały wysokie podobieństwo, a punkty daleko od siebie będą miały niskie podobieństwo. 
-# Algorytm klasteryzacji będzie następnie używał tej macierzy podobieństwa do grupowania punktów w klastry.
-# Jądrowa funkcja RBF jest często używana w algorytmach klasteryzacji, ponieważ pozwala ona na uzyskanie dobrych wyników dla danych o dużym rozmiarze 
-# lub gdy istnieją silne powiązania między punktami danych. 
-# Jeśli jednak dane są szczególnie skomplikowane lub nieregularne, inne rodzaje jądrowych funkcji podobieństwa mogą działać lepiej.
-
-# Jeśli ustawisz parametr affinity na 'nearest_neighbors' podczas wywoływania funkcji SpectralClustering z biblioteki scikit-learn, 
-# algorytm będzie używał macierzy podobieństwa opartej na najbliższych sąsiadach punktów danych do obliczenia macierzy laplasjanu. 
-# W tym przypadku macierz podobieństwa będzie miała wartość 1 dla par punktów, które są najbliższymi sąsiadami,  a 0 dla pozostałych par punktów.
-
-# Ogólnie rzecz biorąc, algorytm SpectralClustering będzie używał macierzy podobieństwa opartej na najbliższych sąsiadach 
-# do grupowania punktów w klastry w sposób, w którym punkty blisko siebie są bardziej prawdopodobne, że znajdą się w tym samym klasterze. 
-# Ten rodzaj macierzy podobieństwa jest szczególnie przydatny w przypadku danych, w których istnieją silne powiązania między punktami blisko siebie.
-
-# n_clusters: liczba klastrów.
 
 
 model = SpectralClustering(n_clusters=(int)(MAX(cluster_number)), affinity=cluster_method,assign_labels='kmeans')
@@ -91,89 +61,106 @@ labels = model.fit_predict(points)
 
 class Graph:
 
+    """initialize graph
+    """
     def __init__(self, vertices):
-        self.V = vertices # No. of vertices
-        self.graph = [] # default dictionary
-        # to store graph
+        
+        """No. of vertices
+        """
+        self.V = vertices 
 
-    # function to add an edge to graph
+        """default dictionary to store graph
+        """
+        self.graph = [] 
+    """
+    function to add an edge to graph
+    """
     def addEdge(self, u, v, w):
         self.graph.append([u, v, w])
-
-    # A utility function to find set of an element i
-    # (uses path compression technique)
+   
+    """
+    A utility function to find set of an element i (uses path compression technique)
+    """
     def find(self, parent, i):
         if parent[i] == i:
             return i
         return self.find(parent, parent[i])
-
-    # A function that does union of two sets of x and y
-    # (uses union by rank)
+    
+    """
+    A function that does union of two sets of x and y (uses union by rank)
+    """
     def union(self, parent, rank, x, y):
         xroot = self.find(parent, x)
         yroot = self.find(parent, y)
-
-        # Attach smaller rank tree under root of
-        # high rank tree (Union by Rank)
+       
+        """
+        Attach smaller rank tree under root of high rank tree (Union by Rank)
+        """
         if rank[xroot] < rank[yroot]:
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
             parent[yroot] = xroot
-
-        # If ranks are same, then make one as root
-        # and increment its rank by one
+           
+            """
+            If ranks are same, then make one as root and increment its rank by one
+            """
         else:
             parent[yroot] = xroot
             rank[xroot] += 1
 
-    # The main function to construct MST using Kruskal's
-        # algorithm
-    def KruskalMST(self):
 
-         # This will store the resultant MST
+    def KruskalMST(self):
         
-        # An index variable, used for sorted edges
+        """
+        Construct Minimum Spanning Tree (MST) using Kruskal's algorithm
+        """
+        
+        """
+        An index variable, used for sorted edges
+        """
         i = 0
-        
-        # An index variable, used for result[]
+       
+        """
+        An index variable, used for result[]
+        """
         e = 0
 
-        # Step 1: Sort all the edges in
-        # non-decreasing order of their
-        # weight. If we are not allowed to change the
-        # given graph, we can create a copy of graph
         self.graph = sorted(self.graph,
                             key=lambda item: item[2])
 
         parent = []
         rank = []
-
-        # Create V subsets with single elements
+        
+        """
+        Create V subsets with single elements
+        """
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
-
-        # Number of edges to be taken is equal to V-1
+       
+        """
+        Number of edges to be taken is equal to V-1
+        """
         while e < self.V - 1:
 
-            # Step 2: Pick the smallest edge and increment
-            # the index for next iteration
             u, v, w = self.graph[i]
             i = i + 1
             x = self.find(parent, u)
             y = self.find(parent, v)
 
-            
-            # If including this edge doesn't
-            # cause cycle, include it in result
-            # and increment the indexof result
-            # for next edge
+           
+            """
+            If including this edge doesn't cause cycle, include it in result and 
+            increment the indexof result for next edge
+            """
             if x != y:
                 e = e + 1
                 result.append([u, v, w])
                 self.union(parent, rank, x, y)
-            # Else discard the edge
-
+            
+            """
+            Else discard the edge
+            """
         arr = numpy.empty(len(result),dtype=float)
         
         
@@ -196,28 +183,40 @@ class Graph:
         print('\n')  
                                
 
-
+"""creates a graph object with a given number of vertices.
+"""
 graph = Graph(size)
 
+"""are nested loops that iterate over the vertices.
+"""
 for i in range(0,size):
 
     for j in range(0,size):
         
         if(i!=j):
             
-            
+            """calculates the Euclidean distance between the two points represented by the vertices.
+            """
             a = numpy.array([parsedCSV[i][0],parsedCSV[i][1]],dtype='f')
             b = numpy.array([parsedCSV[j][0],parsedCSV[j][1]],dtype='f')
+            """calculate weight
+            """
             weight = numpy.linalg.norm(a-b)
+            """add edge
+            """
             graph.addEdge(i, j, weight) 
  
-
+"""uses MST algorighm on created graph
+"""
 graph.KruskalMST() 
 
 csv_result2 = []
 
 csv_result = numpy.asarray(csv_result)
 
+"""
+this code saves points and their distances to .csv file by iterating through MST result
+"""
 with open(str(path)+"\\distances.csv", 'a',newline='' ) as file:
     writer = csv.writer(file)
     
@@ -228,6 +227,10 @@ with open(str(path)+"\\distances.csv", 'a',newline='' ) as file:
     for px in range (0,size-1):       
         writer.writerow(csv_result[px]+labels[px])
 
+"""
+this code saves points and their clusters to .csv file by iterating through point coordinate
+and their clusters
+"""
 with open(str(path)+"\\clusters.csv", 'a',newline='' ) as file:
     writer = csv.writer(file)
     
@@ -242,7 +245,9 @@ with open(str(path)+"\\clusters.csv", 'a',newline='' ) as file:
     for px in range (0,size-1):       
         writer.writerow(csv_result2[px])        
     
-
+"""
+plots the scatter plot of the points using x and y coordinates and the cluster labels.
+"""
 plt.scatter(points[:, 0], points[:, 1], c=labels,s=25, cmap='viridis')
 
 
